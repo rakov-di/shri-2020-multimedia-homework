6afc5f2168686478e27938a8dae84e40e8b2a895
Dmitry Rakov <rakov.di@gmail.com>
2020-04-29 00:02:41 +0300

    [+] Добавляет функционал контролам управления якростью/контрасностью (js)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Vars
   const controlPanel = document.querySelector('.control-panel');
   const allCameras = controlPanel.querySelector('.all-cameras');
+  const inputs = controlPanel.querySelectorAll('.input')
   let activeVideo = null;
 
   // Event handlers
@@ -9,7 +10,13 @@ document.addEventListener('DOMContentLoaded', function () {
     video.addEventListener('click', openFullScreen);
   });
 
+  inputs.forEach(function (input) {
+    input.addEventListener('input', changeFilterValue);
+  });
+
   allCameras.addEventListener('click', closeFullScreen);
+
+
 
   // Functions
   function openFullScreen(e) {
@@ -17,12 +24,28 @@ document.addEventListener('DOMContentLoaded', function () {
     activeVideo = target;
     target.classList.add('video_fullscreen');
     controlPanel.classList.remove('control-panel_hidden');
+    inputs.forEach(function(input) {
+      input.value=activeVideo.dataset[input.id];
+    })
   }
 
   function closeFullScreen() {
     activeVideo.classList.remove('video_fullscreen');
     activeVideo = null;
     controlPanel.classList.add('control-panel_hidden');
+    inputs.forEach(function(input) {
+      input.value='';
+    })
+  }
+
+  function changeFilterValue(e) {
+    const { target } = e;
+    activeVideo.dataset[target.id] = target.value;
+    updateVideoFilter();
+  }
+
+  function updateVideoFilter() {
+    activeVideo.style.filter = `brightness(${activeVideo.dataset.brightness}%) contrast(${activeVideo.dataset.contrast}%)`;
   }
 })
 
